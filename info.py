0d88283cd54c8ca51f3d6c40f934fffc9dd7236a
ZINAN <97530806+Zinan100@users.noreply.github.com>
2023-05-11 15:05:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,6 +55,7 @@ auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+PORT = environ.get("PORT", "8080")
 USE_AS_BOT = environ.get("USE_AS_BOT", True)
 
 # maximum message length in Telegram
