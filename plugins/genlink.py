cfc234ac677bf1a2a30cbaa209a064e5fb33136c
ZINAN <97530806+Zinan100@users.noreply.github.com>
2023-05-12 22:31:06 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -1,6 +1,6 @@
 import re
 
-from pyrogram import filters, Client, enums
+from pyrogram import filters, Client
 
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, UsernameInvalid, UsernameNotModified
 
@@ -9,6 +9,8 @@ from info import ADMINS, LOG_CHANNEL, FILE_STORE_CHANNEL, PUBLIC_FILE_STORE
 from database.ia_filterdb import unpack_new_file_id
 
 from utils import temp
+
+import enums
 
 import re
 
